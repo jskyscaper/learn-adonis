9faedd7f5a0ebfaea4211eb5ccbb02d2584a30a6
jskyscaper <jonathanarmored@yahoo.co.id>
2024-09-13 14:31:32 +0700

    Register Comment

--- app/controllers/auth/register_controller.ts
+++ app/controllers/auth/register_controller.ts
@@ -4,14 +4,14 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class RegisterController {
   async show({ view }: HttpContext) {
-    return view.render('pages/auth/register')
+    //return view.render('pages/auth/register')
   }
 
   async store({ request, response, auth }: HttpContext) {
     //1. Grab data and validate it
-    const data = await request.validateUsing(registerValidator)
+    //const data = await request.validateUsing(registerValidator)
     //2. Create user
-    const user = await User.create(data)
+    //const user = await User.create(data)
     //3. Login user
     await auth.use('web').login(user)
 
